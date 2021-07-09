23d8d6521041aa5ad6d7f1fabe368c8ffd389c5a
Calvin Whitehurst <calvin@calvinwhitehurst.com>
2021-07-09 15:29:14 -0700

    Finished part 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,11 @@
 import * as React from 'React'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
     return (
-        <main>
-            <title>About Me</title>
-            <h1>About Me</h1>
-            <Link to="/about">Back to Home</Link>
-            <p>Hi there! This is the "About" page.  It is built on Gatsby.</p>
-        </main>
+        <Layout pageTitle="About Me">
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </Layout>
     )
 }
 
